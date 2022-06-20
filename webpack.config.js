a6234c84a6821c9fd4bf5cb5882e2d0d5695a11d
sdedieu <dedieu.sylvain38@gmail.com>
2022-06-20 18:58:01 +0200

    add cors

--- webpack.config.js
+++ webpack.config.js
@@ -78,6 +78,11 @@ module.exports = () => {
     devServer: {
       port: 4202,
       historyApiFallback: true,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+      }
     },
   };
 };
